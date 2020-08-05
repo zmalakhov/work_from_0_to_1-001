3aa1cb7e7fde0f2ddf05a38fd58b2527e699ebbc
zmalakhov <zm.13.15.zm@gmail.com>
2020-08-05 11:57:26 +0300

    0003 - slider finish

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -2,7 +2,16 @@ const gulp = require('gulp');
 
 // Копируем все шрифты из папки dev в dist
 
+// module.exports = function fonts() {
+//   return gulp.src('dev/static/fonts/**/*.*')
+//     .pipe(gulp.dest('dist/static/fonts'))
+// };
 module.exports = function fonts() {
+  gulp.src('dev/static/css/**/*.*')
+      .pipe(gulp.dest('dist/static/css'))
+  gulp.src('dev/static/js/vendor/**/*.*')
+      .pipe(gulp.dest('dist/static/js/vendor'))
+
   return gulp.src('dev/static/fonts/**/*.*')
     .pipe(gulp.dest('dist/static/fonts'))
 };
